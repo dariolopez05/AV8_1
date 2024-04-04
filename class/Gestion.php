0267c19e68e7dd200810f905bc6c08bd9e281fcf
Dario Lopez <aodariolopez@gmail.com>
2024-04-04 18:55:54 +0200

    opcional con fallos

--- class/Gestion.php
+++ class/Gestion.php
@@ -18,7 +18,28 @@ class Gestion extends Connection {
     }
 
     function getFavourites($array){
+        $conn = $this->getConn();
+        $output = "";
+        $brands = [];
+        foreach ($array as $brandId) {
+            $query = "SELECT `customerId`, `brandId` FROM `brandCustomer` WHERE `brandId` = '$brandId'";
+            $result = mysqli_query($conn, $query);
+            $brands = $result->fetch_array(MYSQLI_ASSOC);
+            $customerId = $brands['customerId'];
 
+            $query = "SELECT `brandId`, `brandName` FROM `brands` WHERE `brandId` = '$brandId'";
+            $result = mysqli_query($conn, $query);
+            $brand = $result->fetch_array(MYSQLI_ASSOC);
+            $brandName = $brand['brandName'];
+
+            $query = "SELECT `customerId`, `customerName` FROM `customers` WHERE `customerId` = '$customerId'";
+            $result = mysqli_query($conn, $query);
+            $clientes = $result->fetch_array(MYSQLI_ASSOC);
+            $customerName = $clientes['customerName'];
+            
+            $output .= "<tr> <td>$customerId</td> <td>$customerName</td> <td>$brandName</td> </tr>";
+        }
+        return $output;
     }
 }
 
--- favoritos.php
+++ favoritos.php
@@ -3,9 +3,35 @@
 session_start();
 require_once "autoloader.php";
 $brandsId = $_POST['brands'];
-var_dump($brandsId);
 
 $data= new Gestion;
-
+$favoritos = $data->getFavourites($brandsId);
 
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Tabla de Clientes y Marcas</title>
+</head>
+<body>
+
+<table>
+  <thead>
+    <tr>
+      <th>ID Cliente</th>
+      <th>Nombre de Cliente</th>
+      <th>Nombre de Marca</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php  echo $favoritos; ?>
+  </tbody>
+</table>
+<a href="marcas.php">Volver</a>
+
+</body>
+</html>
